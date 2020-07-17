cfb180baf9d0a8083ad95dd4d22cfea11a722b7b
int\felixeckl <felixeckl@outlook.de>
2020-07-18 00:21:03 +0200

    UI optimizations

--- PasswordManagement/ViewModel/Base/NotifyPropertyChanged.cs
+++ PasswordManagement/ViewModel/Base/NotifyPropertyChanged.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using PasswordManagement.Annotations;
 
 namespace PasswordManagement.ViewModel.Base
 {
--- PasswordManagement/ViewModel/MainViewModel.cs
+++ PasswordManagement/ViewModel/MainViewModel.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Timers;
 using System.Windows.Input;
-using PasswordManagement.Annotations;
 using PasswordManagement.Backend.Binary;
 using PasswordManagement.Backend.Security;
 using PasswordManagement.View;
