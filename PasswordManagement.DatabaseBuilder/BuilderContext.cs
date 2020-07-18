bf2ccd349cfcf378e7b8c018ff929cac86d01911
int\felixeckl <felixeckl@outlook.de>
2020-07-18 13:31:41 +0200

    Added Database Builder

---
+++ PasswordManagement.DatabaseBuilder/BuilderContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace PasswordManagement.DatabaseBuilder
+{
+    public class BuilderContext<T> : DbContext where T : class
+    {
+        private readonly SqlConnectionStringBuilder builder;
+        private Assembly[] assemblies;
+
+        public BuilderContext(Assembly[] assemblies, string serverName, string databaseName, string userName, string password)
+        {
+            builder = new SqlConnectionStringBuilder()
+            {
+                InitialCatalog = databaseName,
+                DataSource = serverName,
+                UserID = userName,
+                Password = password,
+            };
+
+            this.assemblies = assemblies;
+        }
+
+        public BuilderContext(Assembly[] assemblies, string serverName, string databaseName)
+        {
+            builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = serverName,
+                InitialCatalog = databaseName,
+                IntegratedSecurity = true
+            };
+
+            this.assemblies = assemblies;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(builder.ToString());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            TableBuilder tableBuilder = new TableBuilder();
+            List<Type> models = tableBuilder.GetModels<T>(assemblies);
+            tableBuilder.BuildTables(models, modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ PasswordManagement.DatabaseBuilder/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace PasswordManagement.DatabaseBuilder
+{
+    public class Database
+    {
+        public async Task Build<T>(bool deleteExisting, params Assembly[] assemblies) where T : class
+        {
+            BuilderContext<T> context = new BuilderContext<T>(assemblies ,"MARS", "PasswordManagement");
+            
+            if (deleteExisting)
+            {
+                await context.Database.EnsureDeletedAsync();
+            }
+
+            await context.Database.EnsureCreatedAsync();
+            await context.SaveChangesAsync();
+        }
+    }
+}
